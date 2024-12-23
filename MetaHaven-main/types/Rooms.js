0384220780b06d47269966c9c6792cf66bfae025
Anisha <129502276+anishayh@users.noreply.github.com>
2024-12-24 00:43:30 +0530

    Initial commit

---
+++ MetaHaven-main/types/Rooms.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.RoomType = void 0;
+var RoomType;
+(function (RoomType) {
+    RoomType["LOBBY"] = "lobby";
+    RoomType["PUBLIC"] = "metahaven";
+    RoomType["CUSTOM"] = "custom";
+})(RoomType || (exports.RoomType = RoomType = {}));
